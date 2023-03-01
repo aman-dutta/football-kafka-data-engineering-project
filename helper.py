166a67bea6e176c5b96899a7fe9fd3d8229fe5ad
aman-dutta <amandutta004@gmail.com>
2023-03-01 18:11:51 +0530

    Calling and saving csv file

--- helper.py
+++ helper.py
@@ -7,3 +7,13 @@ def jsonWrite(json,keyword):
             print("An exception occured")
         else:
             print("Successfully created file named: " + fileName + " in the current directory")
+
+def csvWrite(csv,keyword):
+    fileName = keyword + ".csv"
+    with open(fileName, "w") as outfile:
+        try:
+            outfile.write(csv)
+        except:
+            print("An exception occured")
+        else:
+            print("Successfully created file named: " + fileName + " in the current directory")
--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from search import *
 from helper import *
 
 def main():
-    jsonData = searchKeyword("ManchesterUnited")
-    jsonWrite(jsonData,"ManchesterUnited")
-    jsonFootballData = searchKeywordWithTimeRange("Football", "2023-02-01","2023-02-02")  # For keyword with Time Range
-    jsonWrite(jsonFootballData,"Football")
+    searchKeyword("ManchesterUnited")
+    # searchKeywordWithTimeRange("Football", "2023-02-01","2023-02-02")  # For keyword with Time Range
     
 
 if __name__ == "__main__":
